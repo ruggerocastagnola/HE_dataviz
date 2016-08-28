406bffb23d91c116e9f0b1783126a8d7fef50b8f
ruggerocastagnola <ruggerocastagnola@gmail.com>
2016-08-28 20:44:13 +0200

    on the train

--- js/script.js
+++ js/script.js
@@ -11,10 +11,54 @@ var newSearch = {
   keywords: []
 };
 
+var vizSettings = {
+  rows: 4,
+  cols: 2
+};
+
 (function($){
 
   $(function(){
     'use strict';
+
+    // cache jquery sel
+    var $window = $(window);
+    var $dataFrame = $('#data-frame');
+    var $researchEmotions = $('#research-emotions');
+    var $researchKeywords = $('#research-keywords');
+    var $researchSubmit = $('#research-submit');
+    var $researchSubmitWrapper = $('#research-submit-wrapper');
+    var $newSearchToggle = $('#new-search-toggle');
+    var $labels = $('a:not(#research-submit');
+    var $allLinks = $('a');
+    var $infoboxEmotions = $('#research-emotions p');
+    var $infoboxKeywords = $('#research-keywords p');
+      
+
+    // get stylesheet for css injection
+    var stylesheet = (function(){
+      var styles = document.styleSheets;
+      for(var i = 0; i < styles.length; i++) {
+        if(styles[i].href.indexOf('style.css') > -1) return styles[i];
+      }
+    })();
+
+    
+
+    var injectCss = (function(e){
+      $window.off('resize', injectCss);
+      setTimeout(function(){
+        // remember to change accordingly in css under #data-frame p
+        var margin = 7*2;
+        var dim = (($dataFrame.width() / vizSettings.rows) - margin).toString();
+        var styles = 'width: ' + dim + 'px; height: ' + dim + 'px;';
+        stylesheet.addRule('#data-frame p', styles);
+        $window.on('resize', injectCss);
+      }, 2000);
+    })();
+
+    $window.on('resize', injectCss);
+    
 
     // UTILITIES
     // http://stackoverflow.com/questions/728360/how-do-i-correctly-clone-a-javascript-object
@@ -55,7 +99,7 @@ var newSearch = {
     }
 
     var clearActive = function(){
-      $('a').removeClass('current-search');
+      $allLinks.removeClass('current-search');
     };
 
     // handle search terms arrays
@@ -66,19 +110,43 @@ var newSearch = {
     };
 
     var clearInfobox = function(){
-      $('#research-emotions p').html('');
-      $('#research-keywords p').html('');
+      $infoboxEmotions.html('');
+      $infoboxKeywords.html('');
     };
 
-    var render = function(text, imgs){
+    var render = function(text, imgs, settings){
       var html = '';
-      for(var i = 0; i < imgs.length; i++){
+      var totItems = settings.rows*settings.cols;
+      var textToDisplay = 0, imgsToDisplay = 0;
+      if(text.length > totItems/2 && imgs.length > totItems/2) {
+        textToDisplay = totItems/2;
+        imgsToDisplay = totItems/2;
+      } else 
+      if(text.length < totItems/2 && imgs.length > totItems/2) {
+        textToDisplay = text.length;  
+        imgsToDisplay = 
+          imgsToDisplay >= totItems - text.length ?
+            totItems - text.length : imgs.length;
+      } else 
+      if(imgs.length < totItems/2 && text.length > totItems/2) {
+        textToDisplay =   
+          textToDisplay >= totItems - imgs.length ?
+            totItems - imgs.length : text.length;
+        imgsToDisplay = imgs.length;
+      } else 
+      if(imgs.length < totItems/2 && text.length < totItems/2) {
+        textToDisplay = text.length;
+        imgsToDisplay = imgs.length;
+      }
+      // build html
+      for(var i = 0; i < imgsToDisplay; i++){
         html += '<img src="' + imgs[i].entity + '" />';
       }
-      for(var i = 0; i < text.length; i++){
+      for(var i = 0; i < textToDisplay; i++){
         html += '<p>' + text[i].content + '</p>';
       }
-      $('#data-frame').html(html);
+      // display
+      $dataFrame.html(html);
     };
     
     var ajaxCall = function(textPath, imgsPath){
@@ -88,7 +156,7 @@ var newSearch = {
           $.ajax({
             url: settings.imgsEndpoint + imgsPath,
             success: function(imgsData, textStatus, jqXHR){
-              render(textData.results, imgsData.results);
+              render(textData.results, imgsData.results, vizSettings);
               
               clearSearchTerms();
               clearInfobox();
@@ -150,20 +218,19 @@ var newSearch = {
 
         if(type == 'emotions' && data.label == 'all') {
           // clean
-          var $p = $('#research-emotions p');
-          appendNewSearchLabel(data, $p, 'clear');
+          appendNewSearchLabel(data, $infoboxEmotions, 'clear');
 
         } else {
           var isAll = contains(newSearch.emotions, {'label': 'all'}, 'label');
 
           if(isAll) {
             // clear
-            var $p = $('#research-' + type + ' p');
-            appendNewSearchLabel(data, $p, 'clear');
+            var $infobox = $('#research-' + type + ' p');
+            appendNewSearchLabel(data, $infobox, 'clear');
           } else {
             // append
-            var $p = $('#research-' + type + ' p');
-            appendNewSearchLabel(data, $p, 'append');
+            var $infobox = $('#research-' + type + ' p');
+            appendNewSearchLabel(data, $infobox, 'append');
           }
         }
       } else {
@@ -222,14 +289,37 @@ var newSearch = {
       }  
     };
 
+    
+
+    // interactions
+    var toggleNewSearch = function(e){
+      $(e.currentTarget).toggleClass('active');
+      $researchEmotions.toggleClass('active');
+      $researchKeywords.toggleClass('active');
+      $researchSubmitWrapper.toggleClass('active');
+    };
+
     // setup
-    $('a:not(#research-submit').on('click', function(e){ onClick(e, newSearch); });
-    $('#research-submit').on('click', function(e){ update(e, newSearch); });
+    // dims
+    //vizSettings
+
+
+    // event listeners
+    $labels.on('click', 
+      function(e){ 
+        if( $newSearchToggle.hasClass('active') ) {
+          onClick(e, newSearch);
+        } 
+      });
+    $researchSubmit.on('click', function(e){ update(e, newSearch); });
 
     newSearch.emotions.push({label: 'all'});
     newSearch.keywords.push({label:"lifeless", researchTwitter:"123", researchInsta:"153"});
     ajaxCall('researches=123', 'researches=153');
     setActive('all');
     setActive('lifeless');
+
+    // interactions
+    $newSearchToggle.on('click', toggleNewSearch);
   });
 })(jQuery);
