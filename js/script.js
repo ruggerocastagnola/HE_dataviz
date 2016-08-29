f9c751b128c963a74f024f6f80214256933416fa
ruggerocastagnola <ruggerocastagnola@gmail.com>
2016-08-29 02:00:04 +0200

    intro

--- js/script.js
+++ js/script.js
@@ -1,25 +1,43 @@
-var settings = {
-  textEndpoint: 'http://164.132.225.138/~caketest/testcake/api/getContentByComfortEnergy?',
-  imgsEndpoint: 'http://164.132.225.138/~caketest/testcake/api/getImagesByComfortEnergy?'
-};
-var currentSearch = {
-  emotions: [],
-  keywords: []
-};
-var newSearch = {
-  emotions: [],
-  keywords: []
-};
-
-var vizSettings = {
-  rows: 4,
-  cols: 2
-};
-
 (function($){
 
   $(function(){
     'use strict';
+
+    // settings
+    var settings = {
+      textEndpoint: 'http://164.132.225.138/~caketest/testcake/api/getContentByComfortEnergy?',
+      imgsEndpoint: 'http://164.132.225.138/~caketest/testcake/api/getImagesByComfortEnergy?'
+    };
+    /*
+    var firstSearch = {
+      emotions: ['all'],
+      keywords: ['lifeless']
+    };
+    */
+    var currentSearch = {
+      emotions: [],
+      keywords: []
+    };
+    var newSearch = {
+      emotions: [],
+      keywords: []
+    };
+
+    var vizSettings = {
+      rows: 8,
+      cols: 4
+    };
+    var currentItems = {
+      text: [],
+      imgs: [],
+      activeText: [],
+      activeImgs: [],
+      usedText: [],
+      usedImgs: []
+    };
+
+    var standByTimer;
+    var standByStartUpTime = 60000;
 
     // cache jquery sel
     var $window = $(window);
@@ -33,35 +51,12 @@ var vizSettings = {
     var $allLinks = $('a');
     var $infoboxEmotions = $('#research-emotions p');
     var $infoboxKeywords = $('#research-keywords p');
+    var $viz = $('#viz');
+    var $intro = $('#intro');
       
-
-    // get stylesheet for css injection
-    var stylesheet = (function(){
-      var styles = document.styleSheets;
-      for(var i = 0; i < styles.length; i++) {
-        if(styles[i].href.indexOf('style.css') > -1) return styles[i];
-      }
-    })();
-
-    
-
-    var injectCss = (function(e){
-      $window.off('resize', injectCss);
-      setTimeout(function(){
-        // remember to change accordingly in css under #data-frame p
-        var margin = 7*2;
-        var dim = (($dataFrame.width() / vizSettings.rows) - margin).toString();
-        var styles = 'width: ' + dim + 'px; height: ' + dim + 'px;';
-        stylesheet.addRule('#data-frame p', styles);
-        $window.on('resize', injectCss);
-      }, 2000);
-    })();
-
-    $window.on('resize', injectCss);
-    
-
-    // UTILITIES
+    // utilities
     // http://stackoverflow.com/questions/728360/how-do-i-correctly-clone-a-javascript-object
+    // clone object
     var clone = function(obj) {
       if (null == obj || "object" != typeof obj) return obj;
       var copy = obj.constructor();
@@ -70,14 +65,14 @@ var vizSettings = {
       }
       return copy;
     }
-    // check if array contains element
+    // check if array contains element by key
     var contains = function(arr, needle, key) {
       for(var i = 0; i < arr.length; i++){
         if(arr[i][key] == needle[key]) return true;
       }
       return false;
     };
-    // remove element from array
+    // remove element from array by key
     var removeFromArray = function(arr, el, key){
       for(var i = 0; i < arr.length; i++){
         if(arr[i][key] == el[key]) {
@@ -87,17 +82,18 @@ var vizSettings = {
       }
     };
 
-    // handle labels of the UI
+    // handle label of the UI
     var setActive = function(label){
       $('a[data-label="'+ label + '"]').addClass('current-search');
     };
 
+    // handle all labels of the UI
     var setActiveMultiple = function(arr, key){
       for(var i = 0; i < arr.length; i++){
         setActive(arr[i][key]);
       }
     }
-
+    // clean up all highlights from the labels in the UI
     var clearActive = function(){
       $allLinks.removeClass('current-search');
     };
@@ -109,13 +105,14 @@ var vizSettings = {
       newSearch.keywords = [];
     };
 
+    // clean content of the infobox for the new search
     var clearInfobox = function(){
       $infoboxEmotions.html('');
       $infoboxKeywords.html('');
     };
 
+    // render new contents in the data frame at the center
     var render = function(text, imgs, settings){
-      var html = '';
       var totItems = settings.rows*settings.cols;
       var textToDisplay = 0, imgsToDisplay = 0;
       if(text.length > totItems/2 && imgs.length > totItems/2) {
@@ -126,26 +123,63 @@ var vizSettings = {
         textToDisplay = text.length;  
         imgsToDisplay = 
           imgsToDisplay >= totItems - text.length ?
-            totItems - text.length : imgs.length;
+            totItems - text.length : imgs.length > totItems ?
+              totItems : imgs.length;
       } else 
       if(imgs.length < totItems/2 && text.length > totItems/2) {
         textToDisplay =   
           textToDisplay >= totItems - imgs.length ?
-            totItems - imgs.length : text.length;
+            totItems - imgs.length : text.length > totItems ?
+              totItems: text.length;
         imgsToDisplay = imgs.length;
       } else 
       if(imgs.length < totItems/2 && text.length < totItems/2) {
         textToDisplay = text.length;
         imgsToDisplay = imgs.length;
       }
-      // build html
-      for(var i = 0; i < imgsToDisplay; i++){
-        html += '<img src="' + imgs[i].entity + '" />';
-      }
-      for(var i = 0; i < textToDisplay; i++){
-        html += '<p>' + text[i].content + '</p>';
-      }
+
+      // update cached selection
+      currentItems.text = clone(text);
+      currentItems.imgs = clone(imgs);
+      currentItems.activeText = [];
+      currentItems.activeImgs = [];
+      currentItems.usedText = [];
+      currentItems.usedImgs = [];
+      
       // display
+      append(textToDisplay, imgsToDisplay, text, imgs);
+    };
+
+    // http://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+    var getRandomInt = function(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    var getImg = function(imgs, excluded) {
+      var i = getRandomInt(0, imgs.length-1);
+      return contains(imgs, imgs[i], 'entity') ? imgs[i] : getImg(imgs, excluded);
+    };
+
+    var append = function(textToDisplay, imgsToDisplay, text, imgs) {
+      // build html
+      var html = '';
+      var totItems = textToDisplay + imgsToDisplay;
+      var textCounter = 0, imgsCounter = 0;
+      for(var i = 0; i < totItems; i++) {
+        if(textCounter < textToDisplay && imgsCounter < imgsToDisplay ) {
+          if( getRandomInt(0,1) == 0 ){
+            var img = getImg(imgs, currentItems.usedImgs);
+            html += '<div style="background-image:url(' + img.entity + ');" /></div>';
+            currentItems.activeImgs.push(img);
+            currentItems.usedImgs.push(img);
+          } else {
+            var singleText = getImg(text, currentItems.usedText);
+            html += '<p>' + singleText.content + '</p>';
+            currentItems.activeText.push(singleText);
+            currentItems.usedText.push(singleText);
+          }
+        }
+      }
       $dataFrame.html(html);
     };
     
@@ -157,11 +191,11 @@ var vizSettings = {
             url: settings.imgsEndpoint + imgsPath,
             success: function(imgsData, textStatus, jqXHR){
               render(textData.results, imgsData.results, vizSettings);
-              
               clearSearchTerms();
               clearInfobox();
               updateInfobox({'label': 'all'}, 'emotions', 'add');
 
+              toggleNewSearch();
               // update selected labels in the UI
               clearActive();
               setActiveMultiple(currentSearch.emotions, 'label');
@@ -205,7 +239,7 @@ var vizSettings = {
 
     var appendNewSearchLabel = function(data, $el, type) {
       type == 'clear' ?
-        $el.html('<span>' + data.label + '</span>'):
+        $el.html('<span>' + data.label + '</span> '):
         $el.append('<span>' + data.label + '</span> ');
       setTimeout(function(){
         $el.find('span:last-child').addClass('visible');
@@ -289,11 +323,33 @@ var vizSettings = {
       }  
     };
 
+    // stand by mode
+    var resetStandByTimer = function(){
+      clearTimeout(standByTimer);
+      standByTimer = setTimeout(function(){
+        $viz.fadeOut();
+        $intro.fadeIn();
+        $window.on('mousemove', clearStandBy);
+      }, standByStartUpTime);
+    }
+
+    var clearStandBy = function(e) {
+      clearTimeout(standByTimer);
+      $window.off('mousemove', clearStandBy);
+      $window.off('mousemove', resetStandByTimer);      
+      setTimeout(function(){
+        $viz.fadeIn();
+        $intro.fadeOut();
+        resetStandByTimer();
+        $window.on('mousemove', resetStandByTimer);
+      }, 250);
+    };
+
     
 
     // interactions
     var toggleNewSearch = function(e){
-      $(e.currentTarget).toggleClass('active');
+      $newSearchToggle.toggleClass('active');
       $researchEmotions.toggleClass('active');
       $researchKeywords.toggleClass('active');
       $researchSubmitWrapper.toggleClass('active');
@@ -314,12 +370,17 @@ var vizSettings = {
     $researchSubmit.on('click', function(e){ update(e, newSearch); });
 
     newSearch.emotions.push({label: 'all'});
-    newSearch.keywords.push({label:"lifeless", researchTwitter:"123", researchInsta:"153"});
-    ajaxCall('researches=123', 'researches=153');
+    newSearch.keywords.push({label:"locate", researchTwitter:"102", researchInsta:"134"});
+    ajaxCall('researches=134', 'researches=102');
+    //{label:"lifeless", researchTwitter:"123", researchInsta:"153"}
+    //ajaxCall('researches=123', 'researches=153');
     setActive('all');
     setActive('lifeless');
 
     // interactions
     $newSearchToggle.on('click', toggleNewSearch);
+
+    $window.on('mousemove', clearStandBy);
+
   });
 })(jQuery);
